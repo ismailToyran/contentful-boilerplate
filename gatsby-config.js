1f3ebcded03c5fbc99f5f0412cf6db3a211a3e39
ismail <ismailtoyran@gmail.com>
2020-05-22 15:04:29 +0300

    Plugins added

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ if (!spaceId || !accessToken) {
 module.exports = {
   siteMetadata: {
     title: 'Gatsby Contentful starter',
+    siteUrl: `https://www.ismailtoyran.com`,
   },
   pathPrefix: '/gatsby-contentful-starter',
   plugins: [
@@ -33,9 +34,48 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `The Cool Application`,
+        short_name: `Cool App`,
+        description: `The application does cool things and makes your life better.`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#fff`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+        cache_busting_mode: 'none',
+        lang: `en`,
+        localize: [
+          {
+            start_url: `/de/`,
+            lang: `de`,
+            name: `Die coole Anwendung`,
+            short_name: `Coole Anwendung`,
+            description: `Die Anwendung macht coole Dinge und macht Ihr Leben besser.`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-offline',
+      options: {
+         workboxConfig: {
+            globPatterns: ['**/*']
+         }
+      }
+   },
+   {
+    resolve: `gatsby-plugin-styled-components`,
+    options: {
+      // Add any options here
+    },
+   },
   ],
 }
